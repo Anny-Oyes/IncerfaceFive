d4690da4b7297ca83bd3e07094880fa67c677c01
Anny-Oyes <stephanyoyes@gmail.com>
2022-10-07 16:13:02 -0600

    Inferfaz Cinco

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
@@ -8,7 +8,7 @@ import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <View style={styles.subContainer}>
         <Feather name="menu" size={30} style={styles.iconMenu} />
         <Text style={styles.titled}> Any Ecommerce </Text>
@@ -51,7 +51,13 @@ export default function App() {
           <Text style={styles.textAldo}>Aldo</Text>
           <Text>Almond Toe</Text>
           <Text style={styles.textSize}>Size: 39 us</Text>
-          <Text style={styles.textAED}>AED 45</Text>
+          <Text style={styles.textAed}>AED 50</Text>
+          <Text style={styles.textAedTwo}>AED 45</Text>
+
+          <View>
+            <Text style={styles.label}>50% off</Text>
+          </View>
+
         </View>
 
         <View style={styles.counter}>
@@ -68,22 +74,33 @@ export default function App() {
         </View>
       </View>
 
-      <View>
-        <Text>Coupon Code</Text>
-        <View>
-          <Text>APPLY</Text>
+      <View style={styles.cupon}>
+        <Text style={styles.textCupon}>Coupon Code</Text>
+
+        <View style={styles.textApply}>
+
+          <View style={styles.apply}>
+            <Text style={styles.applyTwo}>APPLY</Text>
+          </View>
+
         </View>
       </View>
 
-      <View>
-        <Text>Subtotal:</Text>
-        <Text>Discount:</Text>
-        <Text>VAT:</Text>
-        <Text>Total:</Text>
-        <Text>AED 126.57</Text>
-        <Text>.AED 21.31</Text>
-        <Text>AED 5.26</Text>
-        <Text>AED 125.52</Text>
+      <View style={styles.invoice}>
+        <View>
+          <Text style={styles.textInvoice}>Subtotal:</Text>
+          <Text style={styles.textInvoice}>Discount:</Text>
+          <Text style={styles.textInvoice}>VAT:</Text>
+          <Text style={styles.textInvoice}>Total:</Text>
+        </View>
+
+        <View style={styles.InvoiceTwo}>
+          <Text style={styles.textInvoiceTwo}>AED 126.57</Text>
+          <Text style={styles.textInvoiceTwo}>AED 21.31</Text>
+          <Text style={styles.textInvoiceTwo}>AED 5.26</Text>
+          <Text style={styles.textInvoiceTwo}>AED 125.52</Text>
+        </View>
+
       </View>
 
       <View style={styles.footer}>
@@ -92,13 +109,14 @@ export default function App() {
         </Text>
       </View>
 
+
       <View style={styles.iconFooter}>
         <Ionicons name="md-home" size={30} style={styles.iconsFooterTwo} />
         <FontAwesome name="search" size={30} style={styles.iconsFooterTwo} />
         <FontAwesome name="star" size={30} style={styles.iconsFooterTwo} />
         <FontAwesome name="user" size={30} style={styles.iconsFooterTwo} />
+
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -180,6 +198,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: -85,
     marginLeft: 270,
+    borderWidth: 1,
+    borderColor: '#a0a0a0',
   },
 
   counter: {
@@ -201,6 +221,8 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     marginTop: 35,
     marginLeft: 270,
+    borderWidth: 1,
+    borderColor: '#a0a0a0',
   },
 
   iconLess: {
@@ -242,28 +264,115 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
 
-  footer: {
+  textAed: {
+    fontSize: 10,
+    color: "#909194",
+    textDecorationLine: "line-through",
+    marginTop: 15,
+  },
+
+  textAedTwo: {
+    fontSize: 14,
+    color: "#ff5a5a",
+    fontWeight: "bold",
+    marginTop: -18,
+    marginLeft: 50,
+  },
+
+  label: {
+    width: 50,
+    height: 15,
+    textAlign: "center",
+    backgroundColor: "#333333",
+    marginLeft: -100,
+    marginTop: -90,
+    color: "#ffffff",
+    fontSize: 10,
+  },
+
+  cupon: {
+    backgroundColor: "#f5f5f5",
+    width: "52%",
+    height: 50,
+    marginLeft: "8%",
+    marginTop: 10,
+    flexDirection: "row",
+  },
+
+  textCupon: {
+    marginLeft: 12,
+    color: "#909194",
+    marginTop: 12,
+  },
+
+  textApply: {
     position: "absolute",
+    marginLeft: "119%",
+    marginTop: 2,
+    height: 50,
+  },
+
+  apply: {
+    borderColor: "#333333",
+    borderWidth: 2,
+    backgroundColor: "#ffffff",
+    width: 80,
+    height: 50,
+  },
+
+  applyTwo: {
+    textAlign: "center",
+    fontSize: 18,
+    marginTop: 8,
+  },
+
+  invoice: {
+    width: "82%",
+    height: 110,
+    backgroundColor: "#f5f5f5",
+    marginLeft: "8%",
+    marginTop: 10,
+  },
+
+  textInvoice: {
+    marginTop: 5,
+    marginLeft: 10,
+    color: "#909194",
+  },
+
+  InvoiceTwo: {
+    position: "absolute",
+    marginTop: -1,
+    marginLeft: 220,
+  },
+
+  textInvoiceTwo: {
+    marginTop: 5,
+    color: "#909194",
+  },
+
+  footer: {
+    marginTop: 100,
     height: 50,
     width: "100%",
     backgroundColor: '#333333',
-    bottom: "15%",
+    bottom: "10%",
   },
 
   textFooter: {
     position: "absolute",
     color: "#ffffff",
     alignSelf: "center",
-    marginTop: 15,
+    marginTop: 12,
   },
 
   iconFooter: {
     flexDirection: "row",
     alignSelf: "center",
-    marginTop: -15,
   },
 
   iconsFooterTwo: {
+    marginTop: -60,
     margin: 30,
     color: "#a0a0a0",
   },
